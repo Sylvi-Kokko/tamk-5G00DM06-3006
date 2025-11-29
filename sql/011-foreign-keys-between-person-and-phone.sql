dd6c138ea305195248c0e0e5e09f76a79e79a6e3
unknown <oona.kokko@gmail.com>
2025-11-29 15:10:19 +0200

    011

--- sql/011-foreign-keys-between-person-and-phone.sql
+++ sql/011-foreign-keys-between-person-and-phone.sql
@@ -6,8 +6,7 @@ PRAGMA foreign_keys = ON;
 
 DROP TABLE IF EXISTS dual;
 
-CREATE TABLE dual
-(dummy CHAR NOT NULL);
+CREATE TABLE dual (dummy CHAR NOT NULL);
 
 INSERT INTO dual (dummy) VALUES ('');
 
@@ -19,36 +18,34 @@ SELECT dummy AS 'CREATE TABLE: phone' FROM dual;
 
 DROP TABLE IF EXISTS phone;
 
-CREATE TABLE phone
-(
-      id        INTEGER         NOT NULL
-    , number    VARCHAR(12)    NOT NULL
-    , comment   VARCHAR(100)
+CREATE TABLE phone (
+  id        INTEGER         NOT NULL,
+  number    VARCHAR(12)    NOT NULL,
+  comment   VARCHAR(100),
 
-    , CONSTRAINT phone__id_pk
-      PRIMARY KEY (id)
+  CONSTRAINT phone__id_pk
+    PRIMARY KEY (id),
 );
 
 SELECT dummy AS 'CREATE TABLE: person' FROM dual;
 
 DROP TABLE IF EXISTS person;
 
-CREATE TABLE person
-(
-      id        INTEGER         NOT NULL
-    , last      VARCHAR(150)    NOT NULL
-    , first     VARCHAR(150)    NOT NULL
-    , phone     INTEGER
-    , city      VARCHAR(150)
-    , address   VARCHAR(150)
-    , dob       DATE
+CREATE TABLE person (
+  id        INTEGER         NOT NULL,
+  last      VARCHAR(150)    NOT NULL,
+  first     VARCHAR(150)    NOT NULL,
+  phone     INTEGER,
+  city      VARCHAR(150),
+  address   VARCHAR(150),
+  dob       DATE,
 
-    , CONSTRAINT person__id_pk
-      PRIMARY KEY (id)
+  CONSTRAINT person__id_pk
+    PRIMARY KEY (id),
 
-    , CONSTRAINT person__phone_fk
-      FOREIGN KEY (phone)
-      REFERENCES phone (id)
+  CONSTRAINT person__phone_fk
+    FOREIGN KEY (phone)
+    REFERENCES phone (id),
 );
 
 COMMIT;
