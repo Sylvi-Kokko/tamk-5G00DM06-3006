a75f44a5459e81d159103270ccf6333e2bd2f64b
unknown <oona.kokko@gmail.com>
2025-12-05 16:14:10 +0200

    project

--- sql/022-domain-constraints-product.sql
+++ sql/022-domain-constraints-product.sql
@@ -7,11 +7,9 @@ PRAGMA foreign_keys = ON;
 DROP TABLE IF EXISTS dual;
 
 CREATE TABLE dual (dummy CHAR NOT NULL);
-
 INSERT INTO dual (dummy) VALUES ('');
 
 BEGIN TRANSACTION;
-PRAGMA defer_foreign_keys = ON;
 
 SELECT dummy AS 'CREATE TABLE: product' FROM dual;
 
@@ -42,3 +40,5 @@ VALUES (1, 'Gadget', 'A useful gadget', 50, 100, 120);
 -- Fails, weight not positive
 INSERT INTO product (product_id, name, description, weight, selling_price, net_price)
 VALUES (2, 'Toy', 'A fun widget', -20, 100, 90);
+
+-- End of file
